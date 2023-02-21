290fbb06332c793ba297df21372a6049d75b13e2
rauhaKalle <123322052+rauhaKalle@users.noreply.github.com>
2023-02-21 17:02:23 +0200

    C4.1 Bookstore: Category (Entity)

--- src/main/java/projekti/bookstore/BookstoreApplication.java
+++ src/main/java/projekti/bookstore/BookstoreApplication.java
@@ -1,5 +1,6 @@
 package projekti.bookstore;
 
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 
 import projekti.bookstore.domain.Book;
 import projekti.bookstore.domain.BookRepository;
+import projekti.bookstore.domain.Category;
+import projekti.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -15,12 +18,22 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 	@Bean 
-	public CommandLineRunner demo(BookRepository repository) 
+	public CommandLineRunner demo(BookRepository repository, CategoryRepository cRepository) 
 	{return (args) -> { 
 	Book book1 = new Book("aku ankka", "jaska jokunen",1958,232423423);
 	Book book2 = new Book("Pekka ja Pätkä", "Ressu", 1998 , 78438778);
 	repository.save(book1);
 	repository.save(book2);
+	
+	Category c1 = new Category("Draama");
+	Category c2 = new Category("Komedia");
+	Category c3 = new Category("Trilleri");
+	cRepository.save(c1);
+	cRepository.save(c2);
+	cRepository.save(c3);
+	
+	
+	
 	}; 
 	}
 
