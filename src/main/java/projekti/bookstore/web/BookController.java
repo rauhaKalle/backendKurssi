a447eb24e53e03f0e8d6b23395ea9516c8194b24
rauhaKalle <123322052+rauhaKalle@users.noreply.github.com>
2023-03-09 17:32:12 +0200

    @PreAuthorize delete book

--- src/main/java/projekti/bookstore/web/BookController.java
+++ src/main/java/projekti/bookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -43,6 +44,7 @@ public class BookController {
 		return "booklist";
 	}
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	@PreAuthorize("hasRole('ADMIN')")
 	public String deleteBook(@PathVariable ("id") Long bookId, Model model) {
 
 		repository.deleteById(bookId);
